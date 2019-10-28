cff2b559cbca1f48af2587924323e2298fc6bc63
danny serra <danielserradev@gmail.com>
2019-10-28 14:11:43 -0500

    inital commit

---
+++ TrashCollectorProgram/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TrashCollectorProgram.MvcApplication" Language="C#" %>
---
+++ TrashCollectorProgram/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TrashCollectorProgram.Startup))]
+namespace TrashCollectorProgram
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
